eba4d4722323f5a050a68afebaa5e7b9b0b48c45
Jaideep <guntupallijaideep@gmail.com>
2022-03-02 00:50:49 +0530

    static layout done

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,25 +1,17 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
 
 const Layout = ({ pageTitle, children }) => {
     return (
         <div>
             <title>{pageTitle}</title>
-            <nav className="flex justify-between items-center p-6 px-20">
-                <h2 className="font-bold text-3xl">Mor-Chiraiya</h2>
-                <ul className="flex gap-12 font-semibold text-xl">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                </ul>
-            </nav>
-            <main className="px-20">
+            <NavBar />
+            <main className="px-8 md:px-20 bg-[#fffdf1]">
                 <h1>{pageTitle}</h1>
                 {children}
             </main>
+            <Footer />
         </div>
     );
 };

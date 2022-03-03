a3258a23d5814a27f079c9edaf37e55bdc67eadc
Jaideep <guntupallijaideep@gmail.com>
2022-03-03 11:18:49 +0530

    fixed header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,8 +4,8 @@ import morchiraiya from "../../images/mor-chiraiya-logo.png";
 
 const Header = () => {
     return (
-        <header className="md:absolute md:z-10 w-full">
-            <nav className="flex flex-col gap-4 md:gap-0 md:flex-row justify-between items-center p-4 px-20 bg-[#f2efe0] shadow-lg md:opacity-80">
+        <header className="md:fixed md:z-10 w-full">
+            <nav className="flex flex-col gap-4 md:gap-0 md:flex-row justify-between items-center p-4 px-20 bg-[#F7E2C7] shadow-lg md:opacity-90">
                 <img
                     src={morchiraiya}
                     alt="Logo of the initiative Mor-Chiraiya"

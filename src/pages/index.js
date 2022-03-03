b998238e04e0bc72ff90e4ea43860b138cc56c73
Jaideep <guntupallijaideep@gmail.com>
2022-03-03 11:20:14 +0530

    added to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,17 @@
 import * as React from "react";
 import Layout from "../components/layout/Layout";
-import Hero from "../components/Hero";
+import Hero from "./../components/Hero";
+import CardRow from "../components/CardRow";
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Mor-Chiraya">
             <Hero />
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-                quas aliquam ipsa culpa dolore ipsum adipisci hic consequuntur
-                doloribus porro, eligendi facere velit minima voluptas aliquid
-                similique enim quidem optio!
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-                quas aliquam ipsa culpa dolore ipsum adipisci hic consequuntur
-                doloribus porro, eligendi facere velit minima voluptas aliquid
-                similique enim quidem optio!
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-                quas aliquam ipsa culpa dolore ipsum adipisci hic consequuntur
-                doloribus porro, eligendi facere velit minima voluptas aliquid
-                similique enim quidem optio!
-            </p>
+            {/* <div className="bg-[#94C973] px-10 py-4 text-center">
+                An initiative of Mahasamund Forest Board for sparrow
+                conservation
+            </div> */}
+            <CardRow />
         </Layout>
     );
 };

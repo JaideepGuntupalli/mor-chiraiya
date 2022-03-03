477afa1e5c93b6ee2b162c4068c163e9a3db2762
Jaideep <guntupallijaideep@gmail.com>
2022-03-03 20:48:39 +0530

    Added all to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,27 @@ import * as React from "react";
 import Layout from "../components/layout/Layout";
 import Hero from "./../components/Hero";
 import CardRow from "../components/CardRow";
+import MainCard from "../components/MainCard";
+import TipSection from "../components/TipSection";
+import logo from "../images/gov_logo.png";
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Mor-Chiraya">
             <Hero />
-            {/* <div className="bg-[#94C973] px-10 py-4 text-center">
+            <CardRow />
+            <MainCard />
+            {/* <Form /> */}
+            <TipSection />
+            <div className="bg-[#94C973] px-10 py-4 text-center flex items-center gap-4 justify-center">
+                <img
+                    src={logo}
+                    alt="Mahasamund District Logo"
+                    className="h-20"
+                />
                 An initiative of Mahasamund Forest Board for sparrow
                 conservation
-            </div> */}
-            <CardRow />
+            </div>
         </Layout>
     );
 };

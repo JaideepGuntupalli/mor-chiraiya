76ff9cd6b9126cc5fe922deb8b831eb819021f6f
Jaideep <guntupallijaideep@gmail.com>
2022-03-15 20:48:16 +0530

    changed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+                sans: ["Lato", ...defaultTheme.fontFamily.sans],
                 serif: ["Vesper Libre", ...defaultTheme.fontFamily.serif],
             },
         },

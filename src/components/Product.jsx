1ee833063956914f7a92c2c928687f5fecdbe46a
Jaideep <guntupallijaideep@gmail.com>
2022-03-22 12:33:59 +0530

    fixed mobile view

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -23,15 +23,17 @@ const Product = () => {
 
     return (
         <div
-            className="flex flex-col-reverse gap-14 md:flex-row items-center justify-center mt-8 mb-20"
+            className="flex flex-col-reverse md:gap-14 md:flex-row items-center justify-center mt-8 mb-20"
             id="diy-kit"
         >
-            <div className="w-full md:w-1/2 px-8 mt-16">
-                <h3 className="text-4xl md:text-6xl">DIY Nest Kit</h3>
-                <p className="ml-2 text-xl mt-4">
+            <div className="w-full md:w-1/2 px-8 md:mt-16">
+                <h3 className="hidden md:block text-4xl md:text-6xl">
+                    DIY Nest Kit
+                </h3>
+                <p className="hidden md:block ml-2 text-xl mt-4">
                     Create your own nest for your loved birds
                 </p>
-                <div className="flex-1 mt-12 mb-6 md:mb-16">
+                <div className="md:flex-1 mb-6 md:mb-16">
                     <h2 className="text-2xl mb-8">
                         What will you get as a part of the DIY kit?
                     </h2>
@@ -72,36 +74,47 @@ const Product = () => {
                         </div>
                     </ul>
                 </div>
-                <div className="flex flex-col md:flex-row items-center gap-4 md:gap-0">
-                    <div className="flex flex-col items-center gap-1">
-                        <a
-                            href="https://rzp.io/l/55rZOa1HG0"
-                            className="text-[#6d5b1c] p-4 px-8 bg-yellow-400 rounded-md hover:bg-yellow-300 text-2xl font-bold"
-                        >
-                            ORDER NOW
-                        </a>
-                        <img
-                            src="https://cdn.razorpay.com/static/assets/powered_by_razorpay.png"
-                            alt="Powered by Razorpay"
-                            className="h-6"
-                        />
-                    </div>
+                <div className="hidden md:flex flex-col md:flex-row items-center gap-4 md:gap-0">
+                    <a
+                        href="https://rzp.io/l/55rZOa1HG0"
+                        className="text-[#6d5b1c] p-4 px-8 bg-yellow-400 rounded-md hover:bg-yellow-300 text-2xl font-bold"
+                    >
+                        ORDER NOW
+                    </a>
+                    <button
+                        onClick={copyLink}
+                        className="text-[#253526] mx-8 hover:text-[#29422a] text-xl font-bold underline"
+                    >
+                        Share
+                    </button>
+                </div>
+            </div>
+            <div className="flex flex-col w-5/6 md:w-2/5 justify-center mt-4 md:m-0">
+                <h3 className="text-4xl md:text-6xl md:hidden">DIY Nest Kit</h3>
+                <p className="text-xl mt-4 md:hidden text-zinc-800">
+                    Create your own nest for your loved birds
+                </p>
+                <img src={bag} alt="shipping bag" />
+                <div className="flex gap-2 text-[#253526] mx-auto">
+                    <p className="text-2xl md:text-3xl">₹</p>
+                    <p className="text-4xl md:text-5xl">99</p>
+                    <p className="text-xl md:text-2xl self-end text-[#6d5b1c]">
+                        / KIT
+                    </p>
+                </div>
+                <div className="flex flex-col md:flex-row items-center gap-4 md:gap-0 my-10 md:hidden">
+                    <a
+                        href="https://rzp.io/l/55rZOa1HG0"
+                        className="text-[#6d5b1c] p-4 px-8 bg-yellow-400 rounded-md hover:bg-yellow-300 text-2xl font-bold"
+                    >
+                        ORDER NOW
+                    </a>
                     <button
                         onClick={copyLink}
                         className="text-[#253526] mx-8 hover:text-[#29422a] text-xl font-bold underline pb-6"
                     >
                         Share
                     </button>
-                </div>
-            </div>
-            <div className="flex flex-col w-5/6 md:w-2/5 items-center justify-center">
-                <img src={bag} alt="shipping bag" />
-                <div className="flex gap-2 text-[#253526]">
-                    <p className="text-2xl md:text-3xl">₹</p>
-                    <p className="text-4xl md:text-5xl">99</p>
-                    <p className="text-xl md:text-2xl self-end text-[#6d5b1c]">
-                        / KIT
-                    </p>
                 </div>
             </div>
         </div>
--- src/hi-components/Product.jsx
+++ src/hi-components/Product.jsx
@@ -23,15 +23,17 @@ const Product = () => {
 
     return (
         <div
-            className="flex flex-col-reverse gap-14 md:flex-row items-center justify-center mt-8 mb-20"
+            className="flex flex-col-reverse md:gap-14 md:flex-row items-center justify-center mt-8 mb-20"
             id="diy-kit"
         >
-            <div className="w-full md:w-1/2 px-8 mt-20">
-                <h3 className="text-4xl md:text-6xl">DIY नेस्ट किट</h3>
-                <p className="ml-2 text-xl mt-4">
+            <div className="w-full md:w-1/2 px-8 md:mt-16">
+                <h3 className="hidden md:block text-4xl md:text-6xl">
+                    DIY नेस्ट किट
+                </h3>
+                <p className="hidden md:block ml-2 text-xl mt-4">
                     अपने प्रिय पक्षियों के लिए अपना घोंसला बनाएं
                 </p>
-                <div className="flex-1 mt-12 mb-6 md:mb-16">
+                <div className="md:flex-1 mb-6 md:mb-16">
                     <h2 className="text-2xl mb-8">
                         DIY किट के हिस्से के रूप में आपको क्या मिलेगा?
                     </h2>
@@ -72,36 +74,49 @@ const Product = () => {
                         </div>
                     </ul>
                 </div>
-                <div className="flex flex-col md:flex-row items-center gap-4 md:gap-0">
-                    <div className="flex flex-col items-center gap-1">
-                        <a
-                            href="https://rzp.io/l/55rZOa1HG0"
-                            className="text-[#6d5b1c] p-3 pt-4 px-8 bg-yellow-400 rounded-md hover:bg-yellow-300 text-2xl font-bold"
-                        >
-                            अभी खरीदें
-                        </a>
-                        <img
-                            src="https://cdn.razorpay.com/static/assets/powered_by_razorpay.png"
-                            alt="Powered by Razorpay"
-                            className="h-5"
-                        />
-                    </div>
+                <div className="hidden md:flex flex-col md:flex-row items-center gap-4 md:gap-0">
+                    <a
+                        href="https://rzp.io/l/55rZOa1HG0"
+                        className="text-[#6d5b1c] p-4 px-8 bg-yellow-400 rounded-md hover:bg-yellow-300 text-2xl font-bold"
+                    >
+                        अभी खरीदें
+                    </a>
+                    <button
+                        onClick={copyLink}
+                        className="text-[#253526] mx-8 hover:text-[#29422a] text-xl font-bold underline"
+                    >
+                        दोस्तों को बताओ
+                    </button>
+                </div>
+            </div>
+            <div className="flex flex-col w-5/6 md:w-2/5 justify-center mt-4 md:m-0">
+                <h3 className="text-4xl md:text-6xl md:hidden">
+                    DIY नेस्ट किट
+                </h3>
+                <p className="text-xl mt-4 md:hidden text-zinc-800">
+                    अपने प्रिय पक्षियों के लिए अपना घोंसला बनाएं
+                </p>
+                <img src={bag} alt="shipping bag" />
+                <div className="flex gap-2 text-[#253526] mx-auto">
+                    <p className="text-2xl md:text-3xl">₹</p>
+                    <p className="text-4xl md:text-5xl">99</p>
+                    <p className="text-xl md:text-2xl self-end text-[#6d5b1c]">
+                        / किट
+                    </p>
+                </div>
+                <div className="flex flex-col md:flex-row items-center gap-4 md:gap-0 my-10 md:hidden">
+                    <a
+                        href="https://rzp.io/l/55rZOa1HG0"
+                        className="text-[#6d5b1c] p-4 px-8 bg-yellow-400 rounded-md hover:bg-yellow-300 text-2xl font-bold"
+                    >
+                        अभी खरीदें
+                    </a>
                     <button
                         onClick={copyLink}
                         className="text-[#253526] mx-8 hover:text-[#29422a] text-xl font-bold underline pb-6"
                     >
                         दोस्तों को बताओ
                     </button>
-                </div>
-            </div>
-            <div className="flex flex-col w-5/6 md:w-2/5 items-center justify-center">
-                <img src={bag} alt="shipping bag" />
-                <div className="flex gap-2 text-[#253526]">
-                    <p className="text-2xl md:text-3xl">₹</p>
-                    <p className="text-4xl md:text-5xl">99</p>
-                    <p className="text-xl md:text-2xl self-end text-[#6d5b1c]">
-                        / किट
-                    </p>
                 </div>
             </div>
         </div>
